d5b561a7ac5da96138531473804a1f408dc88a3d
Benjamin Torben-Nielsen <btorbennielsen@gmail.com>
2017-08-02 19:28:12 +0200

    Update update_environment example

--- examples/update_environment/Attracted_by.py
+++ examples/update_environment/Attracted_by.py
@@ -6,7 +6,7 @@ from growth_procs import unit_sample_on_sphere,\
      get_entity,\
      prepare_next_front
 
-L_NORM=2.0
+L_NORM=1.0
 
 def extend_front(front,seed,constellation,interstitial) :
     # attract by a different neuron, get information
@@ -18,8 +18,8 @@ def extend_front(front,seed,constellation,interstitial) :
     else :
         rnd_dir = unit_sample_on_sphere()
         dir_to_entity = direction_to(front,other_entities,what="nearest")
-        dir_to_entity = normalize_length(dir_to_entity,1.0) + rnd_dir*0.3
+        dir_to_entity = normalize_length(dir_to_entity,1.0) + rnd_dir*0.
         new_pos = front.xyz + normalize_length(dir_to_entity,L_NORM)
-    new_front = prepare_next_front(front,new_pos,set_radius=1.5)
+    new_front = prepare_next_front(front,new_pos,set_radius=0.5)
 
     return [new_front]
